31eb4a159b43dc07cac1a946e2e6b51b43481a63
Jonathan King <jonno@jonnoking.com>
2015-03-10 10:58:13 -0700

    0.0.0.1

---
+++ K2Field.SmartObject.Services.CMIS.Sandbox/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace K2Field.SmartObject.Services.CMIS.Sandbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDoSomething = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDoSomething
+            // 
+            this.btnDoSomething.Location = new System.Drawing.Point(136, 45);
+            this.btnDoSomething.Name = "btnDoSomething";
+            this.btnDoSomething.Size = new System.Drawing.Size(112, 23);
+            this.btnDoSomething.TabIndex = 0;
+            this.btnDoSomething.Text = "Do Something";
+            this.btnDoSomething.UseVisualStyleBackColor = true;
+            this.btnDoSomething.Click += new System.EventHandler(this.btnDoSomething_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnDoSomething);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDoSomething;
+    }
+}
+
---
+++ K2Field.SmartObject.Services.CMIS.Sandbox/Form1.cs
@@ -0,0 +1,47 @@
+﻿using DotCMIS;
+using DotCMIS.Client;
+using DotCMIS.Client.Impl;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace K2Field.SmartObject.Services.CMIS.Sandbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDoSomething_Click(object sender, EventArgs e)
+        {
+            //http://chemistry.apache.org/dotnet/getting-started-with-dotcmis.html
+            //http://stackoverflow.com.80bola.com/questions/22979885/dotcmis-createdocument-with-sharepoint-2013/22980169
+            //http://www.c-sharpcorner.com/UploadFile/surya_bg2000/alfresco-integration-with-net-using-cmis/
+
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+
+            parameters[SessionParameter.BindingType] = BindingType.AtomPub;
+            parameters[SessionParameter.AtomPubUrl] = "https://portal.denallix.com/_vti_bin/cmis/rest?getRepositories";
+            parameters[SessionParameter.User] = "administrator@denallix.com";
+            parameters[SessionParameter.Password] = "K2pass!";
+            parameters[SessionParameter.AuthenticationProviderClass] = "DotCMIS.Binding.NtlmAuthenticationProvider";
+
+            SessionFactory factory = SessionFactory.NewInstance();
+            ISession session = factory.GetRepositories(parameters)[0].CreateSession();
+
+            // Get name of first document in repository
+            string firstDocName = session.GetRootFolder().GetChildren().First().Name;
+
+            int i = 0;
+
+        }
+    }
+}
